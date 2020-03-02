afbfa52280a4cbf06b6eb0bffb4efc67de7183e9
PS <zhang.jinghe05016@westone.com.cn>
2020-03-02 15:02:01 +0800

    initiate project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # docker-spring-boot
 hello world project for spring boot and docker
+
+# introduction
+服务端口号： 8484
+servlet context path: /spring-boot
+
+swagger ui: localhost:8484/spring-boot/swagger-ui.html
---
+++ src/main/java/com/ps/docker/DockerApplication.java
@@ -0,0 +1,13 @@
+package com.ps.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class DockerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class,args);
+    }
+}
